b3de4b7929974bc7599ea21625cdb1d6e545c5f4
mohamedamine-code <mohamedaminebensir09@gmail.com>
2025-05-28 11:34:25 +0100

    4 commit

--- lib/pages/cartPage.dart
+++ lib/pages/cartPage.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce_2/compontent/List_cart.dart';
 import 'package:e_commerce_2/models/Shop.dart';
+import 'package:e_commerce_2/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -39,6 +40,16 @@ class Cartpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product = context.watch<Shop>().getCart;
+    void ShowApi(){
+      showDialog(context: context, builder: (BuildContext context){
+        return AlertDialog(
+          content: Text("contect your application to an backend to can user paye !!"),
+          actions: [IconButton(onPressed: (){
+            Navigator.pop(context);
+          }, icon: Text("kk"))],
+        );
+      });
+    }
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -60,7 +71,22 @@ class Cartpage extends StatelessWidget {
             Text('Swipe left to delate item !',style: TextStyle(fontWeight: FontWeight.bold),)
           ],
         );
-      })
+      }),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: GestureDetector(
+        onTap: (){
+          ShowApi();
+        },
+        child: Container(
+          width: 150,
+          height: 60,
+          decoration: BoxDecoration(
+            color: MyTheme.colorScheme.primary,
+            borderRadius: BorderRadius.circular(12)
+          ),
+          child: Center(child: Text('PAY NOW ',style: TextStyle(color: Colors.white),)),
+        ),
+      ),
     );
   }
 }
